20324b6534ed185c98382642b8d066cf72780921
tteguayco <tteguayco@gmail.com>
2022-12-16 13:24:05 +0100

    Improve styles of Sidebar

--- ui/src/components/CustomSidebar.tsx
+++ ui/src/components/CustomSidebar.tsx
@@ -1,20 +1,51 @@
-import { Box, Button, IconButton } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
 import CodeIcon from '@mui/icons-material/Code';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import { ReactNode } from "react";
 
+interface CustomMenuProps {
+  children: ReactNode
+};
 
+const CustomMenu = ({ children }: CustomMenuProps) => (
+  <Menu
+    menuItemStyles={{
+      button: ({ level, active, disabled }) => {
+        // only apply styles on first level elements of the tree
+        if (level === 0)
+          return {
+            color: disabled ? '#B4C6FC' : '#E4ECFE',
+            backgroundColor: active ? '#362F78' : undefined,
+            margin: 10,
+            borderRadius: 10,
+            ":hover": {
+              color: "#E4ECFE",
+              backgroundColor: "#362F78"
+            },
+            fontWeight: "bold",
+            fontSize: 14
+          };
+      },
+    }}
+  >
+    {children}
+  </Menu>
+);
 
 export const CustomSidebar = () => {
   const { collapseSidebar, collapsed } = useProSidebar();
 
   return (
     <Box height="100vh" display="flex">
-      <Sidebar>
-          <Menu>
-            <MenuItem icon={<CodeIcon />}>Query Editor</MenuItem>
-          </Menu>
+      <Sidebar
+        width="280px"
+        backgroundColor="#42389D"
+      >
+          <CustomMenu>
+            <MenuItem icon={<CodeIcon fontSize="small" />}>Query Editor</MenuItem>
+          </CustomMenu>
 
           <IconButton
             aria-label="collapse"
@@ -27,9 +58,9 @@ export const CustomSidebar = () => {
             }}
           >
             {collapsed ? (
-                <KeyboardDoubleArrowRightIcon />
+                <KeyboardDoubleArrowRightIcon color="secondary" />
               ) : (
-                <KeyboardDoubleArrowLeftIcon />
+                <KeyboardDoubleArrowLeftIcon color="secondary" />
             )}
           </IconButton>
       </Sidebar>
--- ui/src/theme/index.ts
+++ ui/src/theme/index.ts
@@ -1,26 +1,12 @@
 import { createTheme } from "@mui/material";
-import { blue, orange, red } from "@mui/material/colors";
 
-declare module '@mui/material/styles' {
-  interface Theme {
-    // primary: string;
-    // secondary: string;
-    status: {
-      danger: string;
-    };
-  }
-  // allow configuration using `createTheme`
-  interface ThemeOptions {
-    status?: {
-      danger?: string;
-    };
-  }
-}
-
-export const theme = createTheme({
-  // primary: red,
-  // secondary: red,
-  status: {
-    danger: orange[500],
+const palette = {
+  primary: {
+    main: '#42389D'
   },
-});
+  secondary: {
+    main: "#B4C6FC"
+  }
+};
+
+export const theme = createTheme({ palette });
